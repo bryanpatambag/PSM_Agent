cb96a74ca32e4d8adea608d770411fc040afab2c
bryanpatambag <bryanpatambag@outlook.com>
2026-06-11 16:58:06 +0800

    Add files via upload

---
+++ PSM_Agent/PSM_Agent/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PSM_Agent
+{
+    public static class Utilities
+    {
+        public static string FormatTimestamp(DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        public static bool IsNullOrEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
